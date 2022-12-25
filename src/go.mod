7974473d4979474ec2edfb4c9547410fe7e85e12
nitish_19 <nitishagrahari@policybazaar.com>
2022-12-25 19:47:55 +0530

    serve route

--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,5 @@
 module ex.com/src
 
 go 1.19
+
+require github.com/nitagr/LoadBalance v0.0.0-20221225134550-cf16a49f52f7 // indirect
--- src/main.go
+++ src/main.go
@@ -2,28 +2,22 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"net/http/httputil"
+	"sync"
 )
 
+func backend(name string, port string) {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Backend server name:%v\n", name)
+		fmt.Fprintf(w, "Response header:%v\n", r.Header)
+	}))
+	http.ListenAndServe(port, mux)
+
+}
 func main() {
-	director := func(request *http.Request) {
-		request.URL.Scheme = "http"
-		request.URL.Host = ":8081"
-	}
+	wg := new(sync.WaitGroup)
+	wg.Add(5)
 
-	rp := &httputil.ReverseProxy{
-		Director: director,
-	}
-
-	s := http.Server{
-		Addr:    ":8080",
-		Handler: rp,
-	}
-
-	if err := s.ListenAndServe(); err != nil {
-		log.Fatal(err.Error())
-	}
-	defer fmt.Println("Server started on PORT ", 8080)
 }
